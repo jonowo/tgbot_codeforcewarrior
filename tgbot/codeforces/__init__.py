289cdaa63ca6dee53baccded32eaf788c82ff517
Jono <jowonowo@gmail.com>
2022-06-21 15:28:03 +0800

    Error detection

--- tgbot/codeforces/__init__.py
+++ tgbot/codeforces/__init__.py
@@ -3,15 +3,28 @@ import requests
 from .models import Contest, ContestPhase
 
 
+class CodeforcesError(Exception):
+    pass
+
+
 class CodeforcesAPI:
     def __init__(self):
         self.base_url = "https://codeforces.com/api"
         self.session = requests.Session()
 
-    def get_contests(self):
-        resp = self.session.get(f"{self.base_url}/contest.list?gym=false")
+    def _request(self, endpoint, *args, **kwargs):
+        resp = self.session.get(f"{self.base_url}/{endpoint}", *args, **kwargs)
         resp.raise_for_status()
-        data = resp.json()["result"]
+        if "Codeforces is temporarily unavailable." in resp.text:
+            raise CodeforcesError("Codeforces is temporarily unavailable.")
+
+        data = resp.json()
+        if data["status"] == "FAILED":
+            raise CodeforcesError(data["comment"])
+        return data["result"]
+
+    def get_contests(self):
+        data = self._request("contest.list", params={"gym": "false"})
         contests = [Contest(**c) for c in data]
         contests = [c for c in contests if c.phase in (ContestPhase.BEFORE, ContestPhase.CODING)]
         contests.sort(key=lambda c: c.startTimeSeconds)
